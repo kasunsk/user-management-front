adb6351a88c2761617fe623ccd9eff29cb4a5476
Kasun Kariyawasam <kasunsk@gmail.com>
2017-11-29 17:08:08 +0530

    updated

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,14 +1,22 @@
-import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import {Injectable} from '@angular/core';
+import {Http, Headers, RequestOptions, Response} from '@angular/http';
 import 'rxjs/add/operator/map'
 
 @Injectable()
 export class AuthenticationService {
-  constructor(private http: Http) { }
+
+  headers = new Headers({"Content-Type": "application/json"});
+  options = new RequestOptions({ headers: this.headers });
+
+  constructor(private http: Http) {
+  }
 
   login(username: string, password: string) {
-    return this.http.post('/api/authenticate', JSON.stringify({ username: username, password: password }))
+
+    return this.http.post('http://localhost:8080/auth/login', JSON.stringify({
+      "username": username,
+      "password": password
+    }), this.options)
       .map((response: Response) => {
         // login successful if there's a jwt token in the response
         let user = response.json();
